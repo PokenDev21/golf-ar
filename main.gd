4a912b5c8efa4aa2d8dcbdcba194ddd83d97ffc1
miraengel <miranda.akrawi.engelbrektsson@hitachigymnasiet.se>
2025-08-28 12:16:22 +0200

    AR funkar

--- main.gd
+++ main.gd
@@ -6,6 +6,8 @@ func _ready():
 	xr_interface = XRServer.find_interface("OpenXR")
 	if xr_interface and xr_interface.is_initialized():
 		print("OpenXR initialized successfully")
+		
+		switch_to_ar()
 
 		# Turn off v-sync for better XR performance
 		DisplayServer.window_set_vsync_mode(DisplayServer.VSYNC_DISABLED)
@@ -31,4 +33,39 @@ func enable_passthrough() -> bool:
 			return false
 
 	get_viewport().transparent_bg = true
+	return true
+
+@onready var viewport : Viewport = get_viewport()
+@onready var environment : Environment = $WorldEnvironment.environment
+
+func switch_to_ar() -> bool:
+	var xr_interface: XRInterface = XRServer.primary_interface
+	if xr_interface:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		if XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_ALPHA_BLEND
+			viewport.transparent_bg = true
+		elif XRInterface.XR_ENV_BLEND_MODE_ADDITIVE in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_ADDITIVE
+			viewport.transparent_bg = false
+	else:
+		return false
+
+	environment.background_mode = Environment.BG_COLOR
+	environment.background_color = Color(0.0, 0.0, 0.0, 0.0)
+	environment.ambient_light_source = Environment.AMBIENT_SOURCE_COLOR
+	return true
+
+func switch_to_vr() -> bool:
+	var xr_interface: XRInterface = XRServer.primary_interface
+	if xr_interface:
+		var modes = xr_interface.get_supported_environment_blend_modes()
+		if XRInterface.XR_ENV_BLEND_MODE_OPAQUE in modes:
+			xr_interface.environment_blend_mode = XRInterface.XR_ENV_BLEND_MODE_OPAQUE
+		else:
+			return false
+
+	viewport.transparent_bg = false
+	environment.background_mode = Environment.BG_SKY
+	environment.ambient_light_source = Environment.AMBIENT_SOURCE_BG
 	return true
